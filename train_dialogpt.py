be0fdcc0ac88c0f4601430a1357125f103fcdd82
Konstantinos Skouras <konst.skouras@gmail.com>
2022-07-02 16:12:18 +0100

    replace tqdm.notebook with tqdm.auto to work both in notebook and terminal

--- train_dialogpt.py
+++ train_dialogpt.py
@@ -37,7 +37,7 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
 from torch.utils.data.distributed import DistributedSampler
-from tqdm.notebook import tqdm, trange
+from tqdm.auto import tqdm, trange
 
 # from pathlib import Path
 
