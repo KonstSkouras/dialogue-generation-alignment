e5e52369f90252cab44a80230d2c7270a697bd55
Konstantinos Skouras <konst.skouras@gmail.com>
2022-07-04 00:05:19 +0100

    Update run_human_normal.py to handle Maptast as well.

--- run_human_normal.py
+++ run_human_normal.py
@@ -1,10 +1,24 @@
+import os
 from utilities import create_result_directories, transform_txt_to_tsv
+
+# dataset = "Switchboard-Corpus"
+dataset = "Maptask-Corpus"
 
 # print(model_res_dict)
 models_list = ["human"]
-human_res_dict = create_result_directories(models_list, to_local_directory=True)
+human_res_dict = create_result_directories(models_list, to_local_directory=True, dataset=dataset)
 
-test_dir = './external/Switchboard-Corpus/swda_data/test/'
-output_dir = human_res_dict["human"]["directory_tsv"]
+if dataset == "Switchboard-Corpus":
+  test_dir = './external/Switchboard-Corpus/swda_data/test/'
+  speakers=["A", "B"]
+else:
+  test_dir = './external/Maptask-Corpus/maptask_data/test/'
+  speakers=["f", "g"]
 
-transform_txt_to_tsv(test_dir, "human", output_dir)
+output_dir = human_res_dict["base_human"]["directory_tsv"]
+
+if(len(os.listdir(output_dir)) > 1):
+  print("Output directory:", output_dir)
+  print("Output directory is not empty. Clear the directory before running the experiment.")
+else:
+  transform_txt_to_tsv(test_dir, "human", output_dir, speakers)
