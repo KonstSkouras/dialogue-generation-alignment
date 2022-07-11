a69701235b25448a49919bdf8743b3bc9f134796
Konstantinos Skouras <konst.skouras@gmail.com>
2022-07-11 18:56:41 +0100

    Update for Maptask

--- create_directories.py
+++ create_directories.py
@@ -3,10 +3,10 @@ from utilities import create_result_directories
 models_list = ["DialoGPT"]
 create_result_directories(models_list, to_local_directory=True, dataset="Switchboard-Corpus", model_type="base")
 create_result_directories(models_list, to_local_directory=True, dataset="Switchboard-Corpus", model_type="finetuned")
-create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus")
+create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus", model_type="base", speakers=["f", "g"])
 # print(model_res_dict)
 
 models_list = ["human"]
 create_result_directories(models_list, to_local_directory=True, dataset="Switchboard-Corpus")
-create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus")
+create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus", model_type="base", speakers=["f", "g"])
 # print(human_res_dict)
