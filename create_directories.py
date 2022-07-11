fe9d7801d1a9a420269f4676f14fdc5a7b2d8ac9
Konstantinos Skouras <konst.skouras@gmail.com>
2022-07-11 23:15:03 +0100

    Add finetuned dialoGPT for Maptask

--- create_directories.py
+++ create_directories.py
@@ -4,6 +4,7 @@ models_list = ["DialoGPT"]
 create_result_directories(models_list, to_local_directory=True, dataset="Switchboard-Corpus", model_type="base")
 create_result_directories(models_list, to_local_directory=True, dataset="Switchboard-Corpus", model_type="finetuned")
 create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus", model_type="base", speakers=["f", "g"])
+create_result_directories(models_list, to_local_directory=True, dataset="Maptask-Corpus", model_type="finetuned", speakers=["f", "g"])
 # print(model_res_dict)
 
 models_list = ["human"]
