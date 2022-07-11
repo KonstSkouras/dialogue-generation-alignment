83f7bc56977c89c54fc4dd7cdf6b096ef86b5e32
Konstantinos Skouras <konst.skouras@gmail.com>
2022-07-11 16:52:04 +0100

    Update run_human_scramble to run for Maptask as well.

--- run_human_scramble.py
+++ run_human_scramble.py
@@ -1,18 +1,23 @@
-from utilities import create_result_directories, get_dialogue_uttr_dict, create_scrambled_dialogues_txt, transform_txt_to_tsv
+import os
+from utilities import create_result_directories, get_dialogue_uttr_dict, create_scrambled_dialogues_txt, transform_txt_to_tsv, get_dataset_settings
 
+# dataset = "Switchboard-Corpus"
+dataset = "Maptask-Corpus"
+
+# print(model_res_dict)
 models_list = ["human"]
-human_res_dict = create_result_directories(models_list, to_local_directory=True)
+human_res_dict = create_result_directories(models_list, to_local_directory=True, dataset=dataset)
 
-test_dir = './external/Switchboard-Corpus/swda_data/test/'
-output_dir = human_res_dict["human"]["directory_tsv"]
+test_dir, speakers = get_dataset_settings(dataset)
+# print("human_res_dict", human_res_dict)
+output_dir_txt = human_res_dict["base_human"]["directory_scrambled_txt"]
+output_dir_tsv = human_res_dict["base_human"]["directory_scrambled_tsv"]
 
 all_dialogue_uttr_dict = get_dialogue_uttr_dict(test_dir)
 
-output_dir = human_res_dict["human"]["directory_scrambled_txt"]
-
-create_scrambled_dialogues_txt(all_dialogue_uttr_dict, output_dir)
-
-transform_txt_to_tsv(human_res_dict["human"]["directory_scrambled_txt"], 
-                     "human", 
-                     human_res_dict["human"]["directory_scrambled_tsv"])
-
+if(len(os.listdir(output_dir_tsv)) > 1):
+  print("Output directory:", output_dir_tsv)
+  print("Output directory is not empty. Clear the directory before running the experiment.")
+else:
+  create_scrambled_dialogues_txt(all_dialogue_uttr_dict, output_dir_txt, speakers) 
+  transform_txt_to_tsv(output_dir_txt, "human", output_dir_tsv)
